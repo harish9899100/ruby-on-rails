51e4a4fe3f4f39ca2513fdcf5de92eed4e310610
harish khileri <harishkhileri98@gmail.com>
2025-10-28 11:02:13 +0530

    practice of 27-oct

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[index show]
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -20,8 +22,29 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
-    def product_params
-      params.expect(product: [ :name ])
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.expect(product: [:name])
+  end
 end
