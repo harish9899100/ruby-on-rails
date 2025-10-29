2db4cfb02a8dd75abb159f8e250bb65d6bb7c10f
harish khileri <harishkhileri98@gmail.com>
2025-10-29 15:25:14 +0530

    add callbacks and then check it's worked

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
   self.table_name = 'my_books'
   validates :title, presence: true
+  after_create :log_new_book
+
+  private
+
+  def log_new_book
+    puts 'A new book was registered'
+  end
 end
