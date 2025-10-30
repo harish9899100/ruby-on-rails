d0b5b871f3cac660a9cff7412761728ae124c1b2
harish khileri <harishkhileri98@gmail.com>
2025-10-30 16:54:26 +0530

    add comments

--- db/migrate/20251029111813_add_details_to_products.rb
+++ db/migrate/20251029111813_add_details_to_products.rb
@@ -1,6 +1,6 @@
 class AddDetailsToProducts < ActiveRecord::Migration[8.0]
   def change
-    add_column :products, :part_number, :string
-    add_column :products, :price, :decimal
+    add_column :products, :price, :decimal, precision: 8, scale: 2, comment: 'The price of the product in USD'
+    add_column :products, :stock_quantity, :integer, comment: 'The current stock quantity of the product'
   end
 end
