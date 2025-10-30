7f25e91b57d91d357e286bf64d663b6eec451dcd
harish khileri <harishkhileri98@gmail.com>
2025-10-30 16:52:43 +0530

    add comments

--- db/migrate/20251026033918_create_users.rb
+++ db/migrate/20251026033918_create_users.rb
@@ -1,11 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
-      t.string :email_address, null: false
-      t.string :password_digest, null: false
-
+    create_table :users, primary_key: %i[id name] do |t|
+      t.string :name
+      t.string :email
       t.timestamps
     end
-    add_index :users, :email_address, unique: true
   end
 end
