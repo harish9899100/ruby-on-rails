c7add68a0eb62522f021b2dcc06d304f64aedcb7
harish khileri <harishkhileri98@gmail.com>
2025-11-14 15:27:01 +0530

    practice 13-14 Nov

--- db/migrate/20251112113943_create_line_items.rb
+++ db/migrate/20251112113943_create_line_items.rb
@@ -1,6 +1,7 @@
 class CreateLineItems < ActiveRecord::Migration[8.0]
   def change
     create_table :line_items do |t|
+      t.string :order
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_12_102533) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_13_110449) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "book_orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.string "order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
