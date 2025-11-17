0fe14b682e342a60ed8d558f881a5d6b4457e698
harish khileri <harishkhileri98@gmail.com>
2025-11-17 13:31:43 +0530

    add costom callback message when new user created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_13_110449) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_17_072146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "birthdays", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "book_orders", force: :cascade do |t|
     t.datetime "created_at", null: false
