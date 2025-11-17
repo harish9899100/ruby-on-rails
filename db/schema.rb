42cdffe2f1b4bdf7971015b0dd4d0ce5538ebe89
harish khileri <harishkhileri98@gmail.com>
2025-11-17 17:04:54 +0530

    add role in schema with the help of migration in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_17_100710) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_17_105258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -73,11 +73,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_17_100710) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
     t.string "name"
+    t.string "role"
+    t.string "password_digest"
   end
 
   add_foreign_key "products", "users"
