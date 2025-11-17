32986ab137077d20f0839f21986a9510aa5cf4dd
harish khileri <harishkhileri98@gmail.com>
2025-11-17 15:52:35 +0530

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
-  after_create :log_new_user
+  validates :name, presence: true
+  before_validation :titleize_name
+  after_validation :log_errors
 
   private
 
-  def log_new_user
-    puts 'A new user was registered'
+  def titleize_name
+    self.name = name.downcase.titleize if name.present?
+    Rails.logger.info("Name titleized to #{name}")
+  end
+
+  def log_errors
+    return unless errors.any?
+
+    Rails.logger.error("Validation failed: #{errors.full_messages.join(', ')}")
   end
 end
