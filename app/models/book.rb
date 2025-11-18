b761a4da80e2c2532d5ca26397dbf301c9d0165d
harish khileri <harishkhileri98@gmail.com>
2025-11-18 13:58:22 +0530

    add touch and has_many

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,17 @@
 class Book < ApplicationRecord
-  self.table_name = 'my_books'
-  validates :title, presence: true
-  after_create :log_new_book
+  belongs_to :library, optional: true, touch: true
+  after_touch do
+    Rails.logger.info('A Book was touched')
+  end
+end
+
+class Library < ApplicationRecord
+  has_many :books
+  after_touch :log_when_books_or_library_touched
 
   private
 
-  def log_new_book
-    puts 'A new book was registered'
+  def log_when_books_or_library_touched
+    Rails.logger.info('Book/Library was touched')
   end
 end
